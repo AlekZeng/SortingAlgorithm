b05adabcbe0907806ef655914e919c45069fd5e6
AlekZeng <37602896+AlekZeng@users.noreply.github.com>
2019-10-10 11:05:20 -0400

    Update main.py
    conversion to class based program

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
-import pprint
+from funcs import Result
+
 
 mydict = []
 
@@ -12,44 +13,13 @@ def LibUpdate():  # compiles csv file to dict
 def EmailPull():  # sample function to test format correctness
   
   for x in mydict:
-    print(x['What is your 1st choice session?'], x['What is your FIRST name?'])
-
-
-def LeastPop():  # Pulls the least popular class based on first chioce
-  choicesA = []
-  countdictA = {}
-  choicesB = []
-  countdictB = {}
-  for x in mydict:
-    choiceA = x["What is your 1st choice session?"]
-    choicesA.append(str(choiceA))
-
-    choiceB= x["What is your 1.2st choice session?"]
-    choicesB.append(str(choiceB))
-
-  reduxchoicesA = list(dict.fromkeys(choicesA))
-  reduxchoicesB = list(dict.fromkeys(choicesB))
-
-  for x in reduxchoicesA:
-    countA = choicesA.count(x)
-    print(f'Class id #{x} has {countA} people who want this class.')
-
-    countdictA[x] = countA
-  print(" ")
-  lowestA = min(countdictA, key=countdictA.get)
-
-  for x in reduxchoicesB:
-    countB = choicesB.count(x)
-    print(f'Class id #{x} has {countB} people who want this class.')
-
-    countdictB[x] = countB
-  
-  print(" ")
-  lowestB = min(countdictB, key=countdictB.get)
-  
-  print(f"Lowest First choice class for block 1 is {lowestA} \nLowest First choice class for block 2 is {lowestB}")  
+    print(x['What is your 1st choice session?'], x['What is your FIRST name?']) 
 
 
 LibUpdate()
-LeastPop()
+
+Result.resultFormat()
+Result.resultInsert("gsbrown@raleighcharterhs.org", 69)
+
+#LeastPop()
 #print(mydict)
