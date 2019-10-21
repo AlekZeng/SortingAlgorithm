78d494c9723e1c075d5773408f80bb70f4db1df1
AlekZeng <37602896+AlekZeng@users.noreply.github.com>
2019-10-21 14:24:02 -0400

    Update main.py
    variables to be manipulated by tf are added, sample assignment loop added for the memes

--- main.py
+++ main.py
@@ -1,25 +1,56 @@
 import csv
-from funcs import Result
-
+from funcs import Result as re, analyze as an
 
 mydict = []
 
 
 def LibUpdate():  # compiles csv file to dict
-  reader = csv.DictReader(open('PF Prefs.csv', 'r'))
-  for line in reader:
-    mydict.append(line)
+    reader = csv.DictReader(open('PF Prefs.csv', 'r'))
+    for line in reader:
+        mydict.append(line)
+
 
 def EmailPull():  # sample function to test format correctness
-  
-  for x in mydict:
-    print(x['What is your 1st choice session?'], x['What is your FIRST name?']) 
+
+    for x in mydict:
+        print(x['What is your 1st choice session?'],
+              x['What is your FIRST name?'])
 
 
 LibUpdate()
 
-Result.resultFormat()
-Result.resultInsert("gsbrown@raleighcharterhs.org", 69)
+tfTop = 5  # To be assigned as tf variable
+tfBot = 4  # To be assigned as tf variable
+"""
+Prioritizing vars
+"""
 
-#LeastPop()
-#print(mydict)
+firstTop = an.tops(tfTop, 1)
+firstBot = an.bots(tfBot, 1)
+
+secTop = an.tops(tfTop, 2)
+secBot = an.bots(tfBot, 2)
+
+triTop = an.tops(tfTop, 3)
+triBot = an.bots(tfBot, 3)
+
+quadTop = an.tops(tfTop, 4)
+quadBot = an.bots(tfBot, 4)
+
+pentTop = an.tops(tfTop, 5)
+pentBot = an.bots(tfBot, 5)
+
+re.resultFormat()
+
+
+for x in firstBot[0]:
+    for a in mydict:
+        if a["What is your 1st choice session?"] == x:
+
+            re.resultInsert(a["Email Address"], x)
+
+for x in secBot[0]:
+    for a in mydict:
+        if a["What is your 2nd choice session?"] == x:
+
+            re.resultInsert(a["Email Address"], x)
